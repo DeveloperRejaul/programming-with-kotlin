a4de6669f2e5681740937147b7df37bc95448163
Rezaul Karim <rezaul@orangetoolz.com>
2025-06-20 18:19:29 +0600

    add

--- http/app/src/main/kotlin/org/example/App.kt
+++ http/app/src/main/kotlin/org/example/App.kt
@@ -1,14 +1,32 @@
 package org.example
 import io.ktor.client.*
-import io.ktor.client.call.body
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
+
+@Serializable
+data class Post(val userId: Int, val id: Int, val title: String, val body: String)
 
 suspend fun main () {
     val client = HttpClient(CIO)
     val response: HttpResponse = client.get("https://jsonplaceholder.typicode.com/posts")
-    println(response.bodyAsText())
+    val jsoStringData = response.bodyAsText()
+
+    // Manually decode JSON
+    val posts: List<Post> = Json.decodeFromString(jsoStringData)
+
+    // 3. Use the data
+    for (post in posts){
+        println(post.id)
+    }
+
+    // Optionally encode it back to JSON
+    // val backToJson = Json.encodeToString(posts)
+    // println(backToJson)
+
+
     client.close()
 }
