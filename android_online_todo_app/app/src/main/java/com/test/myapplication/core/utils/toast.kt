49b25511146ed271ce32efbbb072b20c5b4d3fef
ErfanOrangetoolz <erfan@orangetoolz.com>
2025-07-18 02:12:53 +0600

    add

--- android_online_todo_app/app/src/main/java/com/test/myapplication/core/utils/toast.kt
+++ android_online_todo_app/app/src/main/java/com/test/myapplication/core/utils/toast.kt
@@ -4,5 +4,6 @@ import android.content.Context
 import android.widget.Toast
 
 fun showToast (context: Context, message:String) {
+    // test
     Toast.makeText(context, message, Toast.LENGTH_LONG).show()
 }
